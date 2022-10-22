200e3f6f2708b4efa71868f062b1f23f251b0402
Igor2075 <rogier.van.amersfoort.75@gmail.com>
2022-10-22 16:17:39 -0300

    routes phase 1 completed

--- backend/controllers/itemController.js
+++ backend/controllers/itemController.js
@@ -1,17 +1,23 @@
-const getItems = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+const getItems = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get Items" });
-};
+});
 
-const postItem = (req, res) => {
+const postItem = asyncHandler(async (req, res) => {
+    if (!req.body.itemName) {
+        res.status(400);
+        throw new Error("Please add an Item Name field");
+    }
     res.status(201).json({ message: "New Item" });
-};
+});
 
-const updateItem = (req, res) => {
+const updateItem = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update Item ${req.params.id}` });
-};
+});
 
-const deleteItem = (req, res) => {
+const deleteItem = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete Item ${req.params.id}` });
-};
+});
 
 module.exports = { getItems, postItem, updateItem, deleteItem };
--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -2,12 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getItems, postItem, updateItem, deleteItem } = require("../controllers/itemController");
 
-router.get("/", getItems);
-
-router.post("/", postItem);
-
-router.put("/:id", updateItem);
-
-router.delete("/:id", deleteItem);
+router.route("/").get(getItems).post(postItem);
+router.route("/:id").put(updateItem).delete(deleteItem);
 
 module.exports = router;
